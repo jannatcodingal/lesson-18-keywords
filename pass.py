2b53e7ea392bf92087ae01c2a287f586b8e21ba6
jannatcodingal <jannatrehman007@gmail.com>
2025-04-25 20:57:46 +0500

    pass

---
+++ pass.py
@@ -0,0 +1,12 @@
+for x in range(10):
+    if x%20==0:
+        print("x is found")
+    elif x%15==0:
+        print("x is found")
+    elif x%10==0:
+        print("x is found")
+    elif x%5==0:
+        print("x is found")
+        pass
+    else:
+        print("x is not found")
